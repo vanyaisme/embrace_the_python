851fb74e67bdf657e4638abf394843ef277c9d26
Ivan Lutso <lutsoivan@icloud.com>
2026-01-05 16:00:25 +0100

    List all covered data types with type function

--- learning_python.py
+++ learning_python.py
@@ -14,3 +14,47 @@ height = 1.83 # Assigning a float value to a variable
 is_student = True # Assigning a boolean value to a variable
 print(f'{name} {surname} is {age} years old, {height} meters tall, and student status is {is_student}.') # Using an f-string to format and print multiple variables in a single line.
 
+my_integer_var = 10
+print(type(my_integer_var)) # <class 'int'>
+
+my_float_var = 10.5
+print(type(my_float_var)) # <class 'float'>
+
+my_string_var = "Hello"
+print(type(my_string_var)) # <class 'str'>
+
+my_boolean_var = False
+print(type(my_boolean_var)) # <class 'bool'>
+
+my_dictionary_var = {"key": "value"}
+print(type(my_dictionary_var)) # <class 'dict'>
+
+my_tuple_var = (8, 4, 5)
+print(type(my_tuple_var)) # <class 'tuple'>
+
+my_range_var = range(5)
+print(type(my_range_var)) # <class 'range'>
+
+my_list_var = [2.3, 'Bye', 7, True]
+print(type(my_list_var)) # <class 'list'>
+
+my_set_var = {1, 2, 3}
+print(type(my_set_var)) # <class 'set'>
+
+my_bytes_var = b'example'
+print(type(my_bytes_var)) # <class 'bytes'>
+
+my_frozenset_var = frozenset([4, 5, 6]) 
+print(type(my_frozenset_var)) # <class 'frozenset'>
+
+my_bytearray_var = bytearray(b'example')
+print(type(my_bytearray_var)) # <class 'bytearray'>
+
+my_complex_var = 3 + 4j
+print(type(my_complex_var)) # <class 'complex'>
+
+my_none_var = None
+print(type(my_none_var)) # <class 'NoneType'>
+
+print("End of the program.")
+
