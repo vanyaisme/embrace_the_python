8ef97aaee609641a279ab0168958ae128904a87d
Ivan Lutso <lutsoivan@icloud.com>
2026-01-05 14:18:00 +0100

    Write Hello World! program

---
+++ learning_python.py
@@ -0,0 +1,6 @@
+print("Hello World!") # This is a simple Python program that prints "Hello World!" to the console.
+# You can run this code in any Python environment to see the output.
+# To run the code, save it in a file named learning_python.py and execute it using
+# the command: python learning_python.py
+
+# Enjoy learning Python!
