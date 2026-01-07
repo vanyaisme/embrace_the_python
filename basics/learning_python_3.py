982cfbcd3c4ae36e15e0ac03237a20a4ced6689a
Ivan Lutso <lutsoivan@icloud.com>
2026-01-07 22:50:18 +0100

    Create new file to continue learning

---
+++ basics/learning_python_3.py
@@ -0,0 +1,5 @@
+# =============================================================================
+# LEARNING PYTHON - PART 3
+# =============================================================================
+# This is a continuation of learning_python_2.py and learning_python.py
+# All the files are located in the basics/ directory.
