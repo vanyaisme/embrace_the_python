bb04edad8e8a62203d2c1097717e4bd0bf9e88f1
Ivan Lutso <lutsoivan@icloud.com>
2026-01-12 12:18:59 +0100

    feat: Make script generating range of numbers with space in-between

---
+++ projects/number_pattern_generator.py
@@ -0,0 +1,12 @@
+def number_pattern(n):
+    if not isinstance(n, int):
+        return 'Argument must be an integer value.'
+    elif n < 1:
+        return 'Argument must be an integer greater than 0.'
+    else: 
+        string = ''
+        for num in range(1,n+1):
+            string = f'{string}{str(num)} '
+        return string.strip()
+
+number_pattern(12)
