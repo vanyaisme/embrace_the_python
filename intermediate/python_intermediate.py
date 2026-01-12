77fe5752fe9cc1b8bc9d08596c39704d15a66fc7
Ivan Lutso <lutsoivan@icloud.com>
2026-01-12 05:30:54 +0100

    feat: Add list comprehensions and map(), filter() and sum() functions examples

--- intermediate/python_intermediate.py
+++ intermediate/python_intermediate.py
@@ -379,6 +379,56 @@ for name, id in zip(developers, ids):
     print(f'Name: {name}') # Name: Naomi...
     print(f'ID: {id}') # ID: 1...
 
+# =============================================================================
+# SESSION 21: LIST COMPREHENSIONS AND FUNCTIONS TO WORK WITH LISTS
+# =============================================================================
 
+even_numbers = []
 
+for num in range(21):
+    if num % 2 == 0:
+        even_numbers.append(num)
 
+print(even_numbers)
+
+# More concise way to write this using comprehensions
+# Comprehensions allows to create list in a single line by combining loop and condition directly withing square brackets
+
+even_numbers = [num for num in range(21) if num % 2 == 0]
+print(even_numbers)
+# this approach is more concise and eliminates the need for separate loop and conditional block 
+
+# another example of comprehension 
+numbers = [1, 2, 3, 4, 5]
+result = [(num, 'Even') if num % 2 == 0 else(num, 'Odd') for num in numbers]
+print(result) # [(1, 'Odd'), (2, 'Even'), (3, 'Odd'), (4, 'Even'), (5, 'Odd')]
+
+words = ['tree', 'dog', 'anarchy', 'life', 'cow', 'house']
+
+def is_long_word(word):
+    return len(word) > 4
+
+long_words = list(filter(is_long_word, words))
+print(long_words) # ['anarchy', 'house']
+
+# filter () is used to select elements from an iterable that meet a specific condition
+# filter () accepts a function and an iterable for its arguments 
+
+# map () function take an iterable and applies a functin to each of its elements
+celsius = [0, 10, 20, 30, 40]
+
+def to_fahrenheit(temp):
+    return (temp * 9/5) + 32
+
+fahrenheit = list(map(to_fahrenheit, celsius))
+print(fahrenheit) # [32.0, 50.0, 68.0, 86.0, 104.0]
+
+# sum() function is used to get the sum form an iterable like a list or tuple
+numbers = [5, 10, 15, 20]
+total = sum(numbers)
+print(total) # 50
+
+# we can also pass start argument which sets the initial values for the summation
+numbers = [5, 10, 15, 20]
+total = sum(numbers, 10)
+print(total) # 60
